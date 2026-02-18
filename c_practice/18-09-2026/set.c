f12b5e628b88fd42a59e7f71fa29525bce4d634f
Dhanesh Aka <dhaneshaka@gmail.com>
2026-02-18 17:48:55 +0530

    practiced

---
+++ c_practice/18-09-2026/set.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	int n=128,p1=4,p2=5;
+	n|=(1<<4)|(1<<5);
+	printf("enter value %d\n",n);
+	return 0;
+}
---
+++ c_practice/18-09-2026/setbit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int =m,n
+printf("enter the value of m,n:\n");
+scanf("%d%d",&m,&n);
+n |=(1<<m);
+printf("the value of set bit is :%d\n",n);
+return 0;
+
+}
+
+
